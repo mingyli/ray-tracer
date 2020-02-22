ada40699189f8bcca22288dd8badf1b39432d390
ming li <mingyli34@gmail.com>
2020-02-22 14:56:29 -0800

    Make async

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use futures::future;
 use std::iter;
 
-use indicatif::ProgressIterator;
 use itertools::Itertools;
 use rand::Rng;
 
@@ -13,9 +13,14 @@ use camera::Camera;
 use geometry::{Ray, Vec3};
 use object::{Hittable, World};
 
-fn bounce(ray: &Ray, world: &World, depth: i32) -> Vec3 {
+const NUM_COLS: u32 = 200;
+const NUM_ROWS: u32 = 150;
+const NUM_SAMPLES: usize = 10;
+
+fn bounce(ray: &Ray, world: &World, depth: u32) -> Vec3 {
+    const MAX_DEPTH: u32 = 50;
     if let Some(hit) = world.hit(ray, 0.001, std::f32::MAX) {
-        if depth < 50 {
+        if depth < MAX_DEPTH {
             if let Some((attenuation, scattered)) = hit.material.scatter(&ray, &hit) {
                 attenuation * bounce(&scattered, world, depth + 1)
             } else {
@@ -31,38 +36,50 @@ fn bounce(ray: &Ray, world: &World, depth: i32) -> Vec3 {
     }
 }
 
-fn main() {
-    let nx = 200;
-    let ny = 150;
-    let ns = 10;
+async fn pixel_color(camera: &Camera, world: &World, i: u32, j: u32) -> (u32, u32, u32) {
+    let mut rng = rand::thread_rng();
+    let mut color = iter::repeat_with(|| {
+        let u = (i as f32 + rng.gen::<f32>()) / NUM_COLS as f32;
+        let v = (j as f32 + rng.gen::<f32>()) / NUM_ROWS as f32;
+        camera.ray(u, v)
+    })
+    .take(NUM_SAMPLES)
+    .fold(Vec3::default(), |acc, ray| acc + bounce(&ray, &world, 0));
+    color /= NUM_SAMPLES as f32;
+    color = Vec3::new(color.r().sqrt(), color.g().sqrt(), color.b().sqrt());
+
+    const RGB_SCALAR: f32 = 255.99;
+    let ir = (RGB_SCALAR * color.r()) as u32;
+    let ig = (RGB_SCALAR * color.g()) as u32;
+    let ib = (RGB_SCALAR * color.b()) as u32;
+    (ir, ig, ib)
+}
+
+async fn async_main() {
+    eprintln!("Ray tracing...");
 
     let world = World::random();
-
     let camera = Camera::from_fov(
         Vec3::new(4.0, 1.5, -3.0),
         Vec3::new(0.0, -0.5, 1.0),
         Vec3::new(0.0, 1.0, 0.0),
         90.0,
-        nx as f32 / ny as f32,
+        NUM_COLS as f32 / NUM_ROWS as f32,
     );
 
-    println!("P3\n{} {}\n255", nx, ny);
+    let futures = (0..NUM_ROWS)
+        .rev()
+        .cartesian_product(0..NUM_COLS)
+        .map(|(j, i)| pixel_color(&camera, &world, i, j));
+    let colors = future::join_all(futures).await;
 
-    let mut rng = rand::thread_rng();
-    for (j, i) in (0..ny).rev().cartesian_product(0..nx).progress() {
-        let mut color = iter::repeat_with(|| {
-            let u = (i as f32 + rng.gen::<f32>()) / nx as f32;
-            let v = (j as f32 + rng.gen::<f32>()) / ny as f32;
-            camera.ray(u, v)
-        })
-        .take(ns)
-        .fold(Vec3::default(), |acc, ray| acc + bounce(&ray, &world, 0));
-        color /= ns as f32;
-        color = Vec3::new(color.r().sqrt(), color.g().sqrt(), color.b().sqrt());
-
-        let ir = (255.99 * color.r()) as i32;
-        let ig = (255.99 * color.g()) as i32;
-        let ib = (255.99 * color.b()) as i32;
-        println!("{} {} {}", ir, ig, ib);
+    eprintln!("Writing out pixel RGB values...");
+    println!("P3\n{} {}\n255", NUM_COLS, NUM_ROWS);
+    for (r, g, b) in colors.iter() {
+        println!("{} {} {}", r, g, b);
     }
+}
+
+fn main() {
+    futures::executor::block_on(async_main());
 }
